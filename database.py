1e0d5756438bc8e4f5ebb11aa9e1d05b0903de2e
Lucas Belmont <lucas.belmont@soulasalle.com.br>
2024-12-09 22:53:03 -0300

    Adicionando configurações supabase

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ class Database:
     def _init(self):
         if self._db is None:
             try: 
-                self._db = psycopg.connect(f"host={config.DB_HOST} dbname={config.DB_NAME} user={config.DB_USER} password={config.DB_PASSWORD}")
+                self._db = psycopg.connect(f"host={config.DB_HOST} dbname={config.DB_NAME} port={config.DB_PORT} user={config.DB_USER} password={config.DB_PASSWORD}")
                 print("Conexão feita com sucesso!")
             except psycopg.OperationalError as e:
                 print(f"Erro ao criar a conexão com o banco de dados: [{e}]")
@@ -25,10 +25,6 @@ class Database:
         try:
             conn = self.get_connection()
             cur = conn.cursor()
-            # if type(data) == str:
-            #     cur.execute("INSERT INTO olx (wscp_titulo, wscp_data_hora, wscp_tamanho, wscp_valor, wscp_descricao, wscp_link, wscp_endereco) VALUES %s", (data))
-            # if type(data) == dict:
-            #     cur.execute("INSERT INTO olx (wscp_titulo, wscp_data_hora, wscp_tamanho, wscp_valor, wscp_descricao, wscp_link, wscp_endereco) VALUES (%s, %s, %s, %s, %s, %s, %s)", data["title"], data["description"], data["announce_date"], data["price"], data["size", data["address", data["link"]]])
             cur.execute(query, data)
             print("Inserindo dados no banco...")
             conn.commit()
